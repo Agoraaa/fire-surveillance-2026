4df90ce52dbe6b028110670d30c864f8c6d79eea
agora <mertaliyigit06@gmail.com>
2026-04-13 07:31:17 +0300

    add seed

--- problemgen/automatic_generator.py
+++ problemgen/automatic_generator.py
@@ -8,8 +8,10 @@ from perlin_numpy import (
     generate_perlin_noise_2d, generate_fractal_noise_2d
 )
 
-def _generate_noise(x, mean, std):
-    noise = generate_fractal_noise_2d((1024, 1024), (8, 8), 
+def _generate_noise(x, mean, std, seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+    noise = generate_fractal_noise_2d((1024, 1024), (8, 8),
                                   octaves=8,
                                  persistence=0.2,
                                  lacunarity=2,
@@ -27,8 +29,8 @@ def _generate_noise(x, mean, std):
             res[i].append(noise[closest_x][closest_y])
     return np.array(res)
 
-def generate_risk_map(risk_density = 0.5, width_height = 100):
-    risk_map = _generate_noise(width_height, 0, 1)
+def generate_risk_map(risk_density = 0.5, width_height = 100, seed=None):
+    risk_map = _generate_noise(width_height, 0, 1, seed=seed)
     risk_map -= risk_map.min()
     risk_map /= risk_map.max()
     is_hidden = risk_map < (1-risk_density)
@@ -40,11 +42,11 @@ def generate_risk_map(risk_density = 0.5, width_height = 100):
     risk_map = risk_map**(1/3)
     return risk_map, is_hidden
 
-def generate_instance(output_file, node_xy_count, width_height_kilometers, importance_mean_std, ymn_mean_std, slope_std, wind_speed, risk_density):
-    importances = _generate_noise(node_xy_count, importance_mean_std[0], importance_mean_std[1])
-    ymns = _generate_noise(node_xy_count, ymn_mean_std[0], ymn_mean_std[1])
-    slopes = abs(_generate_noise(node_xy_count, 0, slope_std))
-    risk_statuses, _ = generate_risk_map(risk_density, node_xy_count)
+def generate_instance(output_file, node_xy_count, width_height_kilometers, importance_mean_std, ymn_mean_std, slope_mean_std, wind_speed, risk_density, seed=None):
+    importances = _generate_noise(node_xy_count, importance_mean_std[0], importance_mean_std[1], seed=seed)
+    ymns = _generate_noise(node_xy_count, ymn_mean_std[0], ymn_mean_std[1], seed=seed)
+    slopes = abs(_generate_noise(node_xy_count, slope_mean_std[0], slope_mean_std[1], seed=seed))
+    risk_statuses, _ = generate_risk_map(risk_density, node_xy_count, seed=seed)
     lines = []
     for i in range(width_height_kilometers):
         for j in range(width_height_kilometers):
@@ -72,5 +74,49 @@ def generate_instance(output_file, node_xy_count, width_height_kilometers, impor
 
 
 if __name__ == '__main__':
-    for node_count in [5, 10, 20, 50, 100, 200]:
-        generate_instance(f'problem_{node_count}x{node_count}.xlsx', node_count, node_count, (1, 0.2), (15, 5), 8, 20, 0.55)
+    forest_densities = {
+        "low": (25, 15),
+        "mid": (55, 15),
+        "high": (85, 15)
+    }
+    slopes = {
+        "low": (3, 2),
+        "mid": (10, 5),
+        "high": (25, 10)
+    }
+    moistures = {
+        "critical": (5, 5),
+        "mid": (15, 5),
+        "high": (30, 10)
+    }
+    winds = {
+        "low": 10,
+        "mid": 20,
+        "high": 30
+    }
+    risks = {
+        "high": 0.6,
+        "medium": 0.55,
+        "low": 0.5
+    }
+    
+    seeds = [2640, 45]
+    for seed in seeds:
+        for forest_density in forest_densities:
+            for slope in slopes:
+                for moisture in moistures:
+                    for wind in winds:
+                        for risk in risks:
+                            instance_name = f'{forest_density}F-{slope}S-{moisture}M-{wind}W-{risk}R-seed{seed}'
+                            print(f'Generating {instance_name}')
+                            generate_instance(
+                                f'test/{instance_name}.xlsx',
+                                45,
+                                45,
+                                forest_densities[forest_density],
+                                moistures[moisture],
+                                slopes[slope],
+                                winds[wind],
+                                risks[risk],
+                                seed=seed
+                            )
